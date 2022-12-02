d3505f7709eadbcf8a49e9cd6dd394bc3ca6068b
cgmontea98 <cgmontealegre9804@gmail.com>
2022-12-02 18:14:01 -0500

    actualización de clases hasta con JS hasta el 29/11 falta 30/11 y 1/12 por la hackacton

--- 04js/fundamentos/script/script.js
+++ 04js/fundamentos/script/script.js
@@ -1,3 +1,83 @@
+/*console.log('hola desde un archivo externo');
+
+console.log('window:', window) es un objeto global que encierra todo y en el f12 se ve sus objetos o propiedades */
+
+//Variable no usar kyw de var, let: se puede cambiar el dato
+
+/*var nombreVariableUno = 'hola';
+let nombreVariableDos = 'hello';
+
+// variable constantes NO SE PUEDE CAMBIAR EL DATO
+
+const nombreVariableTres = 'No variable'
+
+console.log('nombreVaribaleUno');
+console.log('nombreVaribaleDos');
+console.log('nombreVaribaleTres');
+
+nombreVariableUno = 'nombreVariableTres';
+nombreVariableDos = 'nombre_variable_tres';
+nombreVariableTres = 'nombre_variable_uno';*/
+
+//Etapas
+// E1: declaración
+
+/*let miVariable; //Declaración de variable
+console.log(miVariable);
+
+// E2: Inicialización
+
+miVariable = 'miVariableXYZ';
+console.log(miVariable);
+
+ No dejar la const sin valor
+const miConstante; 
+console.log(miConstante)*/
+/*
+//TIPOS DE DATOS: PRIMITIVOS Y NO PRIMITIVOS
+
+// TD:P> 
+// 1) String se escriben dentro de "" o '' y son letras
+let tdString = 'String o cadena de texto';
+tdString = 'string con "palabra" entre comillas';
+tdString = '10'
+console.log(tdString);
+
+// 2) Number
+let tdNumber = 10;
+tdNumber = 5;
+console.log(tdNumber);
+
+// 3) Booleano solo tiene dos valores verdadero o falso.
+let tdBooleano = true;
+tdBooleano = false;
+console.log(tdBooleano);
+
+// Operador para saber el tipo de dato que tiene una variable 
+console.log(typeof tdString);
+console.log(typeof tdNumber);
+console.log(typeof tdBooleano),
+*/
+// INTERACCIÓN
+/*
+//Capturar algo que usuario escriba y dejarlo en una variable
+let nombreU = prompt('ingresa tu nombre')
+console.log(nombreU)
+document.write(nombreU)
+let edadU = prompt('ingresa tu edad')
+console.log(edadU)
+document.write(edadU)*/
+/*
+let nombre = prompt('nombre');
+console.log(nombre);
+let apellido = prompt('apellido');
+console.log(apellido);
+let edad = prompt('edad');
+console.log(edad);
+//Concatenar con + " " +
+let nya = nombre + " " + apellido + " " + edad;
+console.log(nya);
+document.write(nya);*/
 // Clase 29/11
 /*
 //Conversión de tipos
@@ -44,9 +124,9 @@ resultado = numero_uno / numero_dos;
 document.write('la división es:', numero_uno + "/" + numero_dos + "=" + resultado + " " + '<br>');*/
 
 // string o cadenas literales, temple string o string literal...
-
+/*
 const nom = 'pepita';
 const apel = 'perez';
 
 const nomApel = `${nom} y el appelido es ${apel}`
-console.log(nomApel)
+console.log(nomApel)*/
